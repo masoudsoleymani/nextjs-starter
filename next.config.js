1f15194d3fd141f58bc88eceaf286f10e6eeab9a
Masoud Soleymani <masoud.soleymani@daimler.com>
2021-06-04 22:56:10 +0100

    Configure dynamic routes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
+const data = require('./utils/projectsData');
+
 module.exports = {
-    trailingSlash: true,
-    exportPathMap: function() {
-      return {
-        '/': { page: '/' }
+  trailingSlash: true,
+  exportPathMap: async function () {
+    const { projects } = data;
+    const paths = {
+      '/': { page: '/' },
+    };
+
+    projects.forEach((project) => {
+      paths[`/project/${project.slug}`] = {
+        page: '/project/[path]',
+        query: { path: project.slug },
       };
-    }
-  };
+    });
+
+    return paths;
+  },
+};

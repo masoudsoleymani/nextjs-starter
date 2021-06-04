1bc512f1756735e14712949ebaf927756749c8fc
Masoud Soleymani <masoud.soleymani@daimler.com>
2021-06-04 23:43:37 +0100

    update the content

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,14 @@ import { projects } from '../utils/projectsData';
 
 const Home = () => (
   <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
+    <h1>What Can You Deploy to Static Apps?</h1>
     <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
         return <SmallCard key={project.id} Icon={Icon} title={project.name} slug={project.slug} />;
       })}
     </div>
+    <h3 className="myname">By Masoud 🤖</h3>
   </div>
 );
 
